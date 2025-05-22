724e258cbe92a597b956f0622815294e90877c6d
spreyo <60149407+spreyo@users.noreply.github.com>
2025-05-22 10:15:41 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+app.get("/", (req, res) =>{
+    
+})
+
+
+app.listen(3001, ()=>{
+    console.log("server running")
+})
